dd5113615dab8575e5e7f1b02eb102dec1cf5e43
Documenter.jl <documenter@juliadocs.github.io>
2023-09-11 18:02:41 +0000

    build based on 209b74c

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = FastRicianLikelihoods","category":"page"},{"location":"#FastRicianLikelihoods","page":"Home","title":"FastRicianLikelihoods","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for FastRicianLikelihoods.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [FastRicianLikelihoods]","category":"page"}]
+}
